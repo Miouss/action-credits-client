44c1a081d14f2214ba02a68f319b05d7b4162c64
Miouss <samir.ghabi@gmail.com>
2023-12-11 11:54:00 +0100

    fix queries

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,7 +2,7 @@ import { MAX_EXECUTED_ACTION_LIMIT, MAX_PENDING_ACTION_LIMIT } from "./misc";
 
 const API = "http://localhost:3001/api";
 
-const API_QUEUE_QUERIES = `?maxPendingActions=${MAX_EXECUTED_ACTION_LIMIT}&maxExecutedActions=${MAX_PENDING_ACTION_LIMIT}`;
+const API_QUEUE_QUERIES = `?maxPendingActions=${MAX_PENDING_ACTION_LIMIT}&maxExecutedActions=${MAX_EXECUTED_ACTION_LIMIT}`;
 
 export const API_USER_ACTIONS = API + `/user-actions` + API_QUEUE_QUERIES;
 export const API_USER_ACTIONS_REFRESH_INTERVAL =
